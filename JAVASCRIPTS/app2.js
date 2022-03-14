4186dde205b0d39cd86a17683d8e18dd539f8f06
서정태 <yyj02124@helixtech.co.kr>
2022-03-14 11:10:15 +0900

    JS연습

--- JAVASCRIPTS/app2.js
+++ JAVASCRIPTS/app2.js
@@ -1,18 +1,18 @@
-const title = document.getElementById("title");
+// const title = document.getElementsByClassName("hello");
 
 // title.innerText = "Got you!";
 
-console.log(title.id);
-console.log(title);
+// console.log(title.id);
+// console.log(title);
 // console.log(title.className);
 /*
 const hellos = document.getElementsByClassName("hello");
 
 console.log(hellos);
 */
-// const title = document.querySelector(".hello h1");
+// const title = document.querySelector(".hello test1");
 
-/*const titles = document.querySelectorAll(".hello h1");
+/*const titles = document.querySelectorAll(".hello test1");
 
 console.log(title);
 
@@ -20,19 +20,57 @@ console.log(titles);
 
 title.style.color = "blue";*/
 
+const test1 = document.querySelector(".hello1");
+
 function handleTitleClick() {
   console.log("title was clicked!");
-  title.style.color = "rgb(0, 0, 255)";
+  test1.style.color = "rgb(0, 0, 255)";
 }
-
 function handleMouseEnter() {
-  title.innerText = "Mouse is here!";
+  test1.innerText = "Mouse is here!";
 }
 
 function handleMouseleave() {
-  title.innerText = "Mouse is gone!";
+  test1.innerText = "Mouse is gone!";
 }
 
-title.addEventListener("click", handleTitleClick);
-title.addEventListener("mouseenter", handleMouseEnter);
-title.addEventListener("mouseleave", handleMouseleave);
+function handleWindowResize() {
+  document.body.style.backgroundColor = "tomato";
+}
+function handleWindowCopy() {
+  alert("copier!");
+}
+function handleWindowOffline() {
+  alert("SOS noWIFI");
+}
+function handleWindowOnline() {
+  alert("All Goooood!!");
+}
+
+test1.onclick = handleTitleClick;
+test1.addEventListener("mouseenter", handleMouseEnter);
+test1.addEventListener("mouseleave", handleMouseleave);
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
+
+// const test2 = document.querySelector(".hello2");
+
+// function handleTitleClick() {
+//   console.log("title was clicked!");
+//   test2.style.color = "rgb(0, 255, 0)";
+// }
+// function handleMouseEnter() {
+//   test2.
+//   test2.innerText = "Mouse is here!";
+// }
+
+// function handleMouseleave() {
+//   test2.innerText = "Mouse is gone!";
+// }
+
+// test2.addEventListener("click", handleTitleClick);
+// test2.addEventListener("mouseenter", handleMouseEnter);
+// test2.addEventListener("mouseleave", handleMouseleave);
